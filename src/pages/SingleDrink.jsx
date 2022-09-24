c3a7d07dce2e82935cee70a3a27317edd7213bf0
GintautasTubilevicius <98910155+GintautasTubilevicius@users.noreply.github.com>
2022-09-24 13:24:10 +0300

    up

--- src/pages/SingleDrink.jsx
+++ src/pages/SingleDrink.jsx
@@ -69,7 +69,7 @@ export default function SingleDrink() {
                                 </div>
                                 <div className="flex flex-row justify-evenly px-3 pb-3 bg-gray-400">
                                     <div className="text-lg text-white opacity-75 hover:opacity-100 pt-3">
-                                        <Link to="/coctails"><button type="button" class="text-white bg-black hover:text-white border border-black hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Go to coctails</button></Link>
+                                        <Link to="/coctails"><button type="button" class="text-white bg-black hover:text-white border border-black hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Go to Coctails</button></Link>
                                     </div>
                                     <div className="text-lg text-white opacity-75 hover:opacity-100 pt-3">
                                         <Link to="/favorites"><button type="button" class="text-white bg-black hover:text-white border border-black hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Favorites</button></Link>
